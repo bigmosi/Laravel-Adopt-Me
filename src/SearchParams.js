1cf817b992cd75f58a48eaa16c129cc302ccc4bb
kinyeramos2021 <kinyeramo@gmail.com>
2021-07-01 18:32:26 +0300

    using select options

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,14 @@
 import { div } from 'prelude-ls';
 import React, { useState } from 'react';
+import { ANIMALS } from "@frontendmasters/pet";
 
 
 const searchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("Dog");
 
     return(
         <div className="search-params">
-            <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
                     Location
@@ -16,6 +17,17 @@ const searchParams = () => {
                     placeholder="Location"
                     onChange={event => setLocation(event.target.value)}
                     />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select id="animal" value={animal} onChange={e => setAnimal(e.target.value)} 
+                    onBlur={e => setAnimal(e.target.value)}
+                    >
+                    <option>All</option>
+                     {ANIMALS.map(animal => {
+                       return <option key={animal} value={animal}>{animal}</option>
+                    })}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
