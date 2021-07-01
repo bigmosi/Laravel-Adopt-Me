91ace7b9204cde6d95f0713c304fe11d620b3401
kinyeramos2021 <kinyeramo@gmail.com>
2021-07-01 14:33:10 +0300

    important information

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
 import React from 'react';
-export default function Pet(props){
-    
-    return(
+export default function Pet({name, animal, breed}) {
+     
+    return (
        <div>
-           <h1>{props.name}</h1>
-           <h2>{props.animal}</h2>
-           <h2>{props.breed}</h2>
+           <h1>Name:{name}</h1>
+           <h2>{animal}</h2>
+           <h2>{breed}</h2>
        </div>
     );
   }
